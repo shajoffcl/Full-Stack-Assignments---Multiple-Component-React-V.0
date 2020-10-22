2d048f447539fc4db0d33c94639b3ebfd9aa8902
Mohd Shaib <shaj.native@gmail.com>
2020-10-22 16:23:48 +0530

    changes done 0.1

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ class App extends Component {
   render() {
     return (
       <>
-        <div id="project-name">Job Portal Website</div>
-        <div id="project-description">
+        <h1 data-ns-test="project-name">Job Portal Website</h1>
+        <p data-ns-test="project-description">
           A job platform for daily workers and as well as non-educational
           background
-        </div>
+        </p>
       </>
     );
   }
