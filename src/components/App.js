06c1804ff9c16d77d0252d961841fc3ea3c0c024
Mohd Shaib <shaj.native@gmail.com>
2020-10-22 16:14:08 +0530

    changes done 0.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div id="project-name">Job Portal Website</div>
+        <div id="project-description">
+          A job platform for daily workers and as well as non-educational
+          background
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
